d9f9a338f6550d48af38630ca82ffbfa015e6c9b
Muazam Kamal <muazam.kamal@gmail.com>
2020-04-16 14:43:23 +0800

    Added handler and config files for the widget

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    status: "EximiaStudios debut game!",
+    activityType: "PLAYING"
+
+    // Available types are:
+    //     PLAYING
+    //     STREAMING
+    //     LISTENING
+    //     WATCHING
+    //     CUSTOM_STATUS
+
+};
