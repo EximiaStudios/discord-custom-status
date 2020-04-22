a681822604f204178f38d05f4fa3c6512e9e3a81
Muazam Kamal <muazam.kamal@gmail.com>
2020-04-22 22:47:56 +0800

    Updated to use database

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,8 +1,41 @@
-const { status, activityType } = require("../config");
+let { status, activityType } = require("../config");
+
+const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize({
+//     dialect: "sqlite",
+//     storage: "../../../../db/status.sqlite"
+// });
+
+const sequelize = new Sequelize("database", "user", "password", {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    // SQLite only
+    storage: "db/status.sqlite",
+});
+
+const StatusDB = sequelize.define("status", {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+    },
+    text: Sequelize.STRING,
+    activity: Sequelize.STRING
+});
 
 module.exports = async client => {
     console.log("status: ready");
 
+    StatusDB.sync();
+
+    const statusFromDB = await StatusDB.findOne({ where: {name: "status"} });
+
+    if (statusFromDB) {
+        status = statusFromDB.get("text");
+        activityType = statusFromDB.get("activity");
+    }
+    
     client.user.setActivity(status, { type: activityType })
         .then(presence => console.log(`Activity set to "${presence.activities[0].name}", type "${presence.activities[0].type}"`))
         .catch(console.error);
