043d64ba78481fca4d8eec63314b5ca02d6af102
Muazam Kamal <muazam.kamal@gmail.com>
2020-04-22 21:59:27 +0800

    Removed unnecessary escape chara

--- handlers/ready.js
+++ handlers/ready.js
@@ -4,6 +4,6 @@ module.exports = async client => {
     console.log("status: ready");
 
     client.user.setActivity(status, { type: activityType })
-        .then(presence => console.log(`Activity set to \"${presence.activities[0].name}\", type \"${presence.activities[0].type}\"`))
+        .then(presence => console.log(`Activity set to "${presence.activities[0].name}", type "${presence.activities[0].type}"`))
         .catch(console.error);
 };
