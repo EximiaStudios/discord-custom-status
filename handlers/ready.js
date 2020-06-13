394cbb28f7e9e8e4369e19baa2989d684407f9b4
Muazam Kamal <muazamkamal.dev@gmail.com>
2020-06-13 11:43:02 +0800

    Refactored db init into different file

--- handlers/ready.js
+++ handlers/ready.js
@@ -2,29 +2,28 @@ let { status, activityType } = require("../config");
 
 var admin = require('firebase-admin');
 var serviceAccount = process.env.FIREBASE_CREDENTIALS || require("../../../core/serviceAccountKey.json");
+const setupDatabase = require("../../../core/firebaseDB");;
 
 module.exports = async client => {
     console.log("status: ready");
 
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: process.env.FIREBASE_URL || "https://eximiabot-dev.firebaseio.com",
-        databaseAuthVariableOverride: {
-            uid: "status-widget",
-        },
-    });
+    setupDatabase(admin, "status-widget");
 
     var db = admin.database();
     var ref = db.ref("status");
 
-    ref.once("value", (data) => {
+    await ref.once("value", (data) => {
         var readStatus = data.val();
 
         status = readStatus.text;
         activityType = readStatus.activity;
+
+        console.log(status, activityType);
     }, (errorObject) => {
         console.log("Status database read failed. " + errorObject.code);
-    });
+    }); // async await
+
+    admin.app().delete();
 
     client.user.setActivity(status, { type: activityType })
         .then(presence => console.log(`Activity set to "${presence.activities[0].name}", type "${presence.activities[0].type}"`))
